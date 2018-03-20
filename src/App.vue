efc8f2e5261e702a8489bd766e796f07cf4f5065
jelmarvanaert <jelmar.vanaert@gmail.com>
2018-03-20 21:46:20 +0100

    changed navigation

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
+      <router-link to="/">Home</router-link>
       <router-link to="/about">About</router-link>
     </div>
     <router-view/>
@@ -9,23 +9,45 @@
 </template>
 
 <style>
+:root{
+  --link-height: 48px;
+}
+
+body, html{
+  padding: 0;
+  margin: 0;
+  background-color: #ecf0f1;
+}
+
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color:#34495e;
 }
 #nav {
-  padding: 30px;
+  height: var(--link-height);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #16a085;
 }
 
 #nav a {
   font-weight: bold;
-  color: #2c3e50;
+  color: #34495e;
+  padding: 0px 15px;
+  height: var(--link-height);
+  line-height: var(--link-height);
+  text-decoration: none;
+}
+
+#nav a:hover {
+  background-color: #1abc9c;
 }
 
 #nav a.router-link-exact-active {
-  color: #42b983;
+  background-color: #1abc9c;
 }
 </style>
